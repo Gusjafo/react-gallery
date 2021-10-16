b07b280475eafa01bda7e5492932aead951b98a2
Gusjafo <gustavojafo@gmail.com>
2021-10-16 12:03:19 -0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -10,24 +10,22 @@ const App = () => {
 
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
-
-      console.log(result);
-
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
+      // console.log(result);
       setItems(result.data);
       setIsLoading(false);
-
     }
     fetchItems()
-  }, [])
+  }, [query])
 
   return (
     <div className="container">
       <Header />
-      <Search />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharactersGrid isLoading={isLoading} items={items} />
     </div>
   );
--- src/components/Characters/CharactersGrid.js
+++ src/components/Characters/CharactersGrid.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import CharactersItem from './CharactersItem'
+import React from 'react';
+import CharactersItem from './CharactersItem';
+import Spinner from '../ui/Spinner';
 
-const CharactersGrid = ({ items, isLoading}) => {
-    return isLoading ? (<h1>Loading...</h1>) : (<section className="cards">
-         {items.map((item) => (
-             <CharactersItem key={item.char_id} item={item}/>
-         ))}
-    </section>)
+const CharactersGrid = ({ items, isLoading }) => {
+    return isLoading ?
+        (<Spinner />) :
+        (<section className="cards">
+            {items.map((item) => (
+                <CharactersItem key={item.char_id} item={item} />
+            ))}
+        </section>)
 }
 
 export default CharactersGrid
